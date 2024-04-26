9bb47f7ef7285a26f848a7173d9390a8df4fcbe8
ishwor dhungana <ishwordhungana03@gmail.com>
2024-04-27 04:58:50 +0600

    register updated

--- duconnect/src/components/posts/Posts.jsx
+++ duconnect/src/components/posts/Posts.jsx
@@ -9,7 +9,7 @@ const Posts = () => {
       name: "Anik",
       userId: 1,
       profilePic:
-        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        "https://i.ibb.co/BCfKZ1r/dud.jpg",
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
       img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
     },
@@ -22,7 +22,15 @@ const Posts = () => {
       desc: "Tenetur iste voluptates dolorem rem commodi voluptate pariatur, voluptatum, laboriosam consequatur enim nostrum cumque! Maiores a nam non adipisci minima modi tempore.",
     },
     {
-      id: 2,
+      id: 3,
+      name: "Mhmdul",
+      userId: 2,
+      profilePic:
+        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      desc: "Tenetur iste voluptates dolorem rem commodi voluptate pariatur, voluptatum, laboriosam consequatur enim nostrum cumque! Maiores a nam non adipisci minima modi tempore.",
+    },
+    {
+      id: 4,
       name: "Mhmdul",
       userId: 2,
       profilePic:
--- duconnect/src/pages/register/Register.jsx
+++ duconnect/src/pages/register/Register.jsx
@@ -1,7 +1,76 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./register.scss";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    userId: "",
+    username: "",
+    name: "",
+    email: "",
+    department: "",
+    registrationNo: "",
+    session: "",
+    hall: "",
+    password: "",
+    profilePic: null,
+  });
+  const [fieldErrors, setFieldErrors] = useState({
+    userId: "",
+    username: "",
+    name: "",
+    email: "",
+    department: "",
+    registrationNo: "",
+    session: "",
+    hall: "",
+    password: "",
+  });
+  const [showSuccessPopup, setShowSuccessPopup] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+    setFieldErrors({
+      ...fieldErrors,
+      [name]: "",
+    });
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setFormData({
+        ...formData,
+        profilePic: file,
+      });
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Check if any field is empty
+    const emptyFields = Object.entries(formData).some(([key, value]) => {
+      if (value === "") {
+        setFieldErrors({
+          ...fieldErrors,
+          [key]: "Please fill in this field.",
+        });
+        return true;
+      }
+      return false;
+    });
+
+   if (!emptyFields) {
+      // Your logic to handle form submission goes here
+      window.alert("Registration Successful!");
+    }
+  };
+
   return (
     <div className="register">
       <div className="card">
@@ -14,20 +83,131 @@ const Register = () => {
           </p>
           <span>Do you have an account?</span>
           <Link to="/login">
-          <button>Login</button>
+            <button>Login</button>
           </Link>
         </div>
         <div className="right">
           <h1>Register</h1>
-          <form>
-            <input type="text" placeholder="Username" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <input type="text" placeholder="Name" />
-            <button>Register</button>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="User ID"
+              name="userId"
+              value={formData.userId}
+              onChange={handleChange}
+            />
+            {fieldErrors.userId && (
+              <p className="error">{fieldErrors.userId}</p>
+            )}
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+            />
+            {fieldErrors.username && (
+              <p className="error">{fieldErrors.username}</p>
+            )}
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+            {fieldErrors.name && (
+              <p className="error">{fieldErrors.name}</p>
+            )}
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+            {fieldErrors.email && (
+              <p className="error">{fieldErrors.email}</p>
+            )}
+            <select
+              name="department"
+              value={formData.department}
+              onChange={handleChange}
+            >
+              <option value="">Select Department</option>
+              <option value="CSE">CSE</option>
+              <option value="IIT">IIT</option>
+              <option value="Pharmacy">Pharmacy</option>
+              <option value="Chemistry">Chemistry</option>
+              <option value="Math">Math</option>
+              <option value="Applied Physics">Applied Physics</option>
+            </select>
+            {fieldErrors.department && (
+              <p className="error">{fieldErrors.department}</p>
+            )}
+            <input
+              type="text"
+              placeholder="Registration No"
+              name="registrationNo"
+              value={formData.registrationNo}
+              onChange={handleChange}
+            />
+            {fieldErrors.registrationNo && (
+              <p className="error">{fieldErrors.registrationNo}</p>
+            )}
+            <input
+              type="text"
+              placeholder="Session"
+              name="session"
+              value={formData.session}
+              onChange={handleChange}
+            />
+            {fieldErrors.session && (
+              <p className="error">{fieldErrors.session}</p>
+            )}
+            <select
+              name="hall"
+              value={formData.hall}
+              onChange={handleChange}
+            >
+              <option value="">Select Hall</option>
+              <option value="IH">IH</option>
+              <option value="SH">SH</option>
+              <option value="FH">FH</option>
+              <option value="SM">SM</option>
+              <option value="JN">JN</option>
+              <option value="HJ">HJ</option>
+              <option value="KL">KL</option>
+              <option value="SK">SK</option>
+            </select>
+            {fieldErrors.hall && (
+              <p className="error">{fieldErrors.hall}</p>
+            )}
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+            {fieldErrors.password && (
+              <p className="error">{fieldErrors.password}</p>
+            )}
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleImageChange}
+              name="profilePic"
+            />
+            <button type="submit">Register</button>
           </form>
         </div>
       </div>
+      {showSuccessPopup && (
+        <div className="success-popup">
+          <p></p>
+        </div>
+      )}
     </div>
   );
 };
